d1c41910676bbca019da1eaae4a96de8b179fc75
TugceAras <aras.tugcee@gmail.com>
2023-02-03 16:58:23 +0300

    updated

--- app/src/main/java/com/tugcearas/newsapp/util/extensions/Extensions.kt
+++ app/src/main/java/com/tugcearas/newsapp/util/extensions/Extensions.kt
@@ -3,6 +3,8 @@ package com.tugcearas.newsapp.util.extensions
 import android.content.Context
 import android.view.View
 import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 fun Context.toastMessage(msg:String) = Toast.makeText(this,msg,Toast.LENGTH_SHORT).show()
-
