107afc08d4744ecf961113bffaf089244d4d9371
TugceAras <aras.tugcee@gmail.com>
2023-02-04 11:07:09 +0300

    updated

--- app/src/main/java/com/tugcearas/newsapp/data/model/NewsResponseModel.kt
+++ app/src/main/java/com/tugcearas/newsapp/data/model/NewsResponseModel.kt
@@ -5,8 +5,10 @@ import androidx.room.ColumnInfo
 data class NewsResponseModel(
     @ColumnInfo(name ="articles" )
     val articles: List<Article>,
+
     @ColumnInfo(name ="status" )
     val status: String,
+
     @ColumnInfo(name ="totalResults" )
     val totalResults: Int
 )
